b262f24fcb8ee37a8ec924128163bb7f0b551df6
mikeku1116 <mikekuforgit@gmail.com>
2022-11-30 00:43:16 +0800

    6.6批次爬取登入型網頁的資料

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 import time
 import credentials
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
 
 options = Options()
@@ -28,3 +29,15 @@ driver.get("https://www.facebook.com/ETtodayMOVIE")
 for i in range(20):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     time.sleep(3)
+
+soup = BeautifulSoup(driver.page_source, "lxml")
+
+posts = soup.find_all("div", {"class": "x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z"})
+
+result = []
+for post in posts:
+    content = post.find("span", {
+                        "class": "x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h"})
+    result.append(content.getText())
+
+print(result)
